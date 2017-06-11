ce9cbb47ef55050f82858536add2341f293812d0
MaksymB <maksym.bilinets+dataflow@gmail.com>
2017-06-11 09:09:25 +0300

    [test] prelude tests moved to a dedicated subfolder

--- test/prelude/test_core.cpp
+++ test/prelude/test_core.cpp
@@ -16,7 +16,7 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with Dataflow++. If not, see <http://www.gnu.org/licenses/>.
 
-#include "tools/io_fixture.h"
+#include "../tools/io_fixture.h"
 
 #include <dataflow/core.h>
 #include <dataflow/introspect.h>
