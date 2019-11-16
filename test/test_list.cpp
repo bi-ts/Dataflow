79be5db25a93043a8f6dc9e271f9c120c8e07693
MaksymB <maksym.bilinets+dataflow@gmail.com>
2019-11-16 10:37:07 +0200

    [list] Fix order of includes in list test

--- test/test_list.cpp
+++ test/test_list.cpp
@@ -16,8 +16,9 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with Dataflow++. If not, see <http://www.gnu.org/licenses/>.
 
-#include <dataflow/introspect.h>
 #include <dataflow/list.h>
+
+#include <dataflow/introspect.h>
 
 #include <boost/test/unit_test.hpp>
 
