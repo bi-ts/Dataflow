3d8c85201a995d934d084bee21cffcbd20574a5b
MaksymB <maksym.bilinets+dataflow@gmail.com>
2019-12-08 23:49:46 +0200

    [list] Move list data type tests to a separate file

---
+++ test/list/test_list_list.cpp
@@ -0,0 +1,185 @@
+
+//  Copyright (c) 2014 - 2019 Maksym V. Bilinets.
+//
+//  This file is part of Dataflow++.
+//
+//  Dataflow++ is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Dataflow++ is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Dataflow++. If not, see <http://www.gnu.org/licenses/>.
+
+#include <dataflow/list.h>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace dataflow;
+
+namespace dataflow_test
+{
+BOOST_AUTO_TEST_SUITE(test_list_list)
+
+BOOST_AUTO_TEST_CASE(test_list)
+{
+  BOOST_CHECK_EQUAL(core::is_trivially_patcheable<listA<int>>::value, false);
+  BOOST_CHECK_EQUAL(core::is_trivially_patcheable<listC<int>>::value, false);
+}
+
+BOOST_AUTO_TEST_CASE(test_listA_ctor)
+{
+  Engine engine;
+
+  const auto a = listA<std::string>{
+    "first", std::string("second"), Var("third"), Const("fourth")};
+}
+
+BOOST_AUTO_TEST_CASE(test_listC_ctor)
+{
+  Engine engine;
+
+  const auto a = listC<std::string>{"first", std::string("second")};
+}
+
+BOOST_AUTO_TEST_CASE(test_listA_make_listA)
+{
+  Engine engine;
+
+  const listA<std::string> b =
+    make_listA("first", std::string("second"), Var("third"), Const("fourth"));
+  const listA<std::string> c =
+    make_listA(Const("first"), std::string("second"), Var("third"), "fourth");
+  const listA<std::string> d =
+    make_listA(Var("first"), std::string("second"), "third", Const("fourth"));
+  const listA<std::string> e =
+    make_listA(std::string("first"), "second", Var("third"), Const("fourth"));
+}
+
+BOOST_AUTO_TEST_CASE(test_listC_make_listC)
+{
+  Engine engine;
+
+  const listC<std::string> b =
+    make_listC("first", std::string("second"), "third");
+  const listC<std::string> c =
+    make_listC(std::string("first"), std::string("second"), "third");
+  const listC<std::string> d =
+    make_listC("first", std::string("second"), std::string("third"));
+  const listC<std::string> e =
+    make_listC(std::string("first"), "second", std::string("third"));
+}
+
+BOOST_AUTO_TEST_CASE(test_listA_equality_inequality_test)
+{
+  Engine engine;
+
+  BOOST_CHECK_EQUAL((listA<int>() == listA<int>()), true);
+  BOOST_CHECK_EQUAL((listA<int>() == listA<int>(1)), false);
+  BOOST_CHECK_EQUAL((listA<int>(2) == listA<int>()), false);
+
+  BOOST_CHECK_EQUAL((listA<int>() != listA<int>()), false);
+  BOOST_CHECK_EQUAL((listA<int>() != listA<int>(1)), true);
+  BOOST_CHECK_EQUAL((listA<int>(2) != listA<int>()), true);
+
+  const auto c1 = Const(1);
+  const auto c2 = Const(2);
+  const auto c3 = Const(3);
+  const auto c4 = Const(4);
+
+  const auto a = listA<int>(c1, c2, c3, c4);
+  const auto b = a;
+  const auto c = listA<int>(1, 2, 3, 4);
+  const auto d = listA<int>(c1, c2, c3, c4);
+
+  BOOST_CHECK_EQUAL((a == b), true);
+  BOOST_CHECK_EQUAL((a != b), false);
+  BOOST_CHECK_EQUAL((a == c), false);
+  BOOST_CHECK_EQUAL((a != c), true);
+  BOOST_CHECK_EQUAL((a == d), true);
+  BOOST_CHECK_EQUAL((a != d), false);
+}
+
+BOOST_AUTO_TEST_CASE(test_listC_insert_into_empty)
+{
+  Engine engine;
+
+  auto xs = listC<std::string>{};
+
+  BOOST_CHECK_EQUAL(xs.size(), 0);
+
+  xs = xs.insert(0, "first");
+
+  BOOST_CHECK_EQUAL(xs.size(), 1);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(first)");
+}
+
+BOOST_AUTO_TEST_CASE(test_listC_insert)
+{
+  Engine engine;
+
+  auto xs = list<std::string>{"1", "2", "3"};
+
+  BOOST_CHECK_EQUAL(xs.size(), 3);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(1 2 3)");
+
+  xs = xs.insert(2, ",");
+
+  BOOST_CHECK_EQUAL(xs.size(), 4);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(1 2 , 3)");
+
+  xs = xs.insert(1, ",");
+
+  BOOST_CHECK_EQUAL(xs.size(), 5);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(1 , 2 , 3)");
+}
+
+BOOST_AUTO_TEST_CASE(test_listC_erase)
+{
+  Engine engine;
+
+  auto xs = make_listC("1", "2", "3", "4", "5");
+
+  BOOST_CHECK_EQUAL(xs.size(), 5);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(1 2 3 4 5)");
+
+  xs = xs.erase(1);
+
+  BOOST_CHECK_EQUAL(xs.size(), 4);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(1 3 4 5)");
+
+  xs = xs.erase(3);
+
+  BOOST_CHECK_EQUAL(xs.size(), 3);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(1 3 4)");
+
+  xs = xs.erase(3);
+
+  BOOST_CHECK_EQUAL(xs.size(), 3);
+  BOOST_CHECK_EQUAL(core::to_string(xs), "list(1 3 4)");
+}
+
+BOOST_AUTO_TEST_CASE(test_listC_subscript)
+{
+  Engine engine;
+
+  auto xs = make_listC("0", "1", "2", "3", "4", "5", "6", "7");
+
+  const auto& cxs = xs;
+
+  BOOST_CHECK_EQUAL(xs[3], "3");
+  BOOST_CHECK_EQUAL(xs[5], "5");
+  BOOST_CHECK_EQUAL(xs[7], "7");
+
+  BOOST_CHECK_EQUAL(cxs[3], "3");
+  BOOST_CHECK_EQUAL(cxs[5], "5");
+  BOOST_CHECK_EQUAL(cxs[7], "7");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+} // dataflow_test
